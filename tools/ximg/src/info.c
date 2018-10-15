936f7a7a0536a60b2e45f904ccb0cc4858fd870a
Tinus Bruins <t.bruins@gmail.com>
2018-10-15 20:41:18 +0200

    Fixed alignment failure in info output

--- tools/ximg/src/info.c
+++ tools/ximg/src/info.c
@@ -37,13 +37,12 @@ int action_info(int argc, const char ** argv){
     for(int index = 0; index < image->header.chunks; index++){
         uint32_t size = image->chunks[index]->size;
         if(size > 1048576){
-            printf("%4.2f MB  ", size / 1048576.0);
-        }else if(image->header.size > 1024){
-            printf("%4.2f kB  ", size / 1024.0);
+            printf("%6.2f MB  ", size / 1048576.0);
+        }else if(size > 1024){
+            printf("%6.2f kB  ", size / 1024.0);
         }else{
             printf("%3d bytes  ", size);
         }
-        //printf("%011d  ", image->chunks[index]->size);
         printf("%.*s  ", 4, &image->chunks[index]->type);
         printf("%04X-%04X  ", image->chunks[index]->id >> 16, image->chunks[index]->id);
         printf("%08X\n", image->chunks[index]->checksum);
